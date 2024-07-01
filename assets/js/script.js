4e2d7939285eae452863761aeef8efa9498c76ba
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-07-01 14:20:30 +0530

    add new pre loader to index page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,20 @@
 	"use strict";
 	
 	//Hide Loading Box (Preloader)
-	function handlePreloader() {
-		if($('.preloader').length){
-			$('.preloader').delay(200).fadeOut(500);
-		}
-	}
+	function firstLoad() {
+        $(".ml_bg-item").addClass("mgl_vis");
+        TweenMax.to(".ml-mask", 1.7, {
+            xPercent: 100,
+            repeat: 0,
+            yoyo: false,
+            repeatDelay: 0.5,
+            ease: Linear.easeNone,
+            onComplete: function () {
+                $(".ml_bg-item , .ml-wrap img").addClass("mgl_clos");
+                $(".main-loader").delay(500).fadeOut(1300);
+            }
+        });
+    }
 	
 	//Update Header Style and Scroll to Top
 	function headerStyle() {
@@ -1137,7 +1146,7 @@
    ========================================================================== */
 	
 	$(window).on('load', function() {
-		handlePreloader();
+		firstLoad();
 	});	
 
 })(window.jQuery);
