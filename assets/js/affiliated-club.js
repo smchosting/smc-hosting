b8299774b88920afc8d3c884e250974e4b1aa18e
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-09 20:01:30 +0530

    add basic dummy for affliliated club

---
+++ assets/js/affiliated-club.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    fetch('assets/json/affiliated-clubs.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        
+        // const container = document.getElementById('lift-image-container');
+        // images.forEach(image => {
+        //   const block = document.createElement('div');
+        //   block.className = 'project-block_one col-lg-4 col-md-6 col-sm-12';
+        //   block.innerHTML = `
+        //     <div class="project-block_one-inner wow fadeInRight" data-wow-delay="300ms" data-wow-duration="1500ms">
+        //       <a href="${image}" title="" class="popup-link">
+        //         <div class="gallery-card">
+        //           <img src="${image}" alt="Image">
+        //         </div>
+        //       </a>
+        //     </div>
+        //   `;
+        //   container.appendChild(block);
+        // });
+
+       
+      })
+      .catch(error => console.error('Error fetching images:', error));
+  });
