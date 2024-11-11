e67bc14848aba8e79811e3aafc37429bfcfc0ce2
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2024-11-11 10:17:02 +0530

    add creating city inputs

--- assets/js/affiliated-club.js
+++ assets/js/affiliated-club.js
@@ -15,15 +15,25 @@ const dataFetching = () =>{
         .catch(error => console.error('Error fetching data:', error));
 }
 
+//creating state inputs
 const createStateInput = (data) =>{
     const uniqueStates = [...new Set(data.map(element => element.state))]; // Extract unique states
     uniqueStates.forEach(state => {
-        console.log(state);
-
         const option = document.createElement('option');
         option.value = state;
         option.textContent = state;
         stateInput.append(option);
+    });
+}
+
+//creating city inputs
+const createCityInput = (data) =>{
+    const uniqueCity = [...new Set(data.map(element => element.city))]; // Extract unique city
+    uniqueCity.forEach(city => {
+        const option = document.createElement('option');
+        option.value = city;
+        option.textContent = city;
+        cityInput.append(option);
     });
 }
 
@@ -34,46 +44,13 @@ const cityInputChange = () =>{
 
 
 
-
-
-
-
-
 $(document).ready(async function () {
-
     fetch('assets/json/affiliated-clubs.json')
         .then(response => response.json())
         .then(data => {
             console.log(data);
             createStateInput(data)
-
-
-
-
-            // const stateInput = $('#state-input');
-            // const uniqueStates = [...new Set(data.map(element => element.state))]; // Extract unique states
-            // uniqueStates.forEach(state => {
-            //     console.log(state);
-
-            //     const option = document.createElement('option');
-            //     option.value = state;
-            //     option.textContent = state;
-            //     stateInput.append(option);
-            // });
-            
-
-            // const cityInput = $('#city-input');
-            // const uniqueCity = [...new Set(data.map(element => element.city))]; // Extract unique city
-            // uniqueCity.forEach(city => {
-            //     console.log(city);
-
-            //     const option = document.createElement('option');
-            //     option.value = city;
-            //     option.textContent = city;
-            //     cityInput.append(option);
-            // });
-            
-
+            createCityInput(data)
         })
         .catch(error => console.error('Error fetching data:', error));
 });
