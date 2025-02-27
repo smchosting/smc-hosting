1c967fe5cdbee438a9a02a795f14bab1f6b8717a
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-02-27 23:07:37 +0530

    add new input for state all

--- assets/js/affiliated-club.js
+++ assets/js/affiliated-club.js
@@ -1,82 +1,103 @@
-
-const stateInput = $('#state-input');
-const cityInput = $('#city-input');
-const tableBody = $('#table-body');
+const stateInput = $("#state-input");
+const cityInput = $("#city-input");
+const tableBody = $("#table-body");
 
 const dataFetching = async () => {
-    try {
-        const response = await fetch('assets/json/affiliated-clubs.json');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+  try {
+    const response = await fetch("assets/json/affiliated-clubs.json");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
-const createStateInput = (data) =>{
-    const uniqueStates = [...new Set(data.map(element => element.State))]; // Extract unique states
-    uniqueStates.forEach(State => {
-        const option = document.createElement('option');
-        option.value = State;
-        option.textContent = State;
-        stateInput.append(option);
-    });
-}
+const createStateInput = (data) => {
+  const uniqueStates = [...new Set(data.map((element) => element.State))]; // Extract unique states
+  uniqueStates.forEach((State, index) => {
+    const option = document.createElement("option");
+    if (index == 0) {
+      const option = document.createElement("option");
+      option.value = "All";
+      option.textContent = "All";
+      stateInput.append(option);
+    }
+    option.value = State;
+    option.textContent = State;
+    stateInput.append(option);
+  });
+};
 
-const tableCreator = (data) =>{
-    $('#table-body').empty();
-    data.forEach(club => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+const tableCreator = (data) => {
+  $("#table-body").empty();
+  data.forEach((club) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
             <td>${club.ClubName}  <br> - ${club.Address}</td>
             <td>${club.State}</td>
             <td>${club.City}</td>
-            <td>${club.PhoneNumbers.join('<br>')}</td>
+            <td>${club.PhoneNumbers.join("<br>")}</td>
         `;
-        tableBody.append(row);
-    });
-}
+    tableBody.append(row);
+  });
+};
 
-const createCityInput = (data, State) =>{
-    $('#city-input').empty();
-    const currentState = data.filter(club => club.State === State);
-    tableCreator(currentState)
-    const uniqueCity = [...new Set(currentState.map(element => element.City))]; // Extract unique City
-    uniqueCity.forEach((City, index) => {
-        const option = document.createElement('option');
-        if(index == 0){
-            const option = document.createElement('option');
-            option.value = "All";
-            option.textContent = "All";
-            cityInput.append(option);
+const createCityInput = (data, State) => {
+  $("#city-input").empty();
+
+  if (State == "All") {
+    tableCreator(data);
+    const uniqueCity = [
+        ...new Set(data.map((element) => element.City)),
+      ]; // Extract unique City
+      uniqueCity.forEach((City, index) => {
+        const option = document.createElement("option");
+        if (index == 0) {
+          const option = document.createElement("option");
+          option.value = "All";
+          option.textContent = "All";
+          cityInput.append(option);
         }
-
+  
         option.value = City;
         option.textContent = City;
         cityInput.append(option);
-        
+      });
+  } else {
+    const currentState = data.filter((club) => club.State === State);
+    tableCreator(currentState);
+    const uniqueCity = [
+      ...new Set(currentState.map((element) => element.City)),
+    ]; // Extract unique City
+    uniqueCity.forEach((City, index) => {
+      const option = document.createElement("option");
+      if (index == 0) {
+        const option = document.createElement("option");
+        option.value = "All";
+        option.textContent = "All";
+        cityInput.append(option);
+      }
+
+      option.value = City;
+      option.textContent = City;
+      cityInput.append(option);
     });
-    console.log(cityInput);
+  }
+};
 
-}
+const stateInputChange = async () => {
+  const data = await dataFetching();
+  createCityInput(data, stateInput.val());
+};
 
-const stateInputChange = async () =>{
-    
-    const data = await dataFetching()
-    createCityInput(data, stateInput.val())
-}
-
-const cityInputChange = async () =>{
-    const data = await dataFetching()
-    const currentCity = data.filter(club => club.City === cityInput.val());
-    tableCreator(currentCity)
-    
-}
+const cityInputChange = async () => {
+  const data = await dataFetching();
+  const currentCity = data.filter((club) => club.City === cityInput.val());
+  tableCreator(currentCity);
+};
 
 $(document).ready(async function () {
-    const data = await dataFetching()
-    console.log("data");
-    console.log(data);
-    createStateInput(data)
-    createCityInput(data, stateInput.val())
+  const data = await dataFetching();
+  createStateInput(data);
+  createCityInput(data, stateInput.val());
 });
